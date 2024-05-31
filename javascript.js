7d47a91e7d738e96ca5b26a9ffe49a04e1c5dc32
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-06-01 01:15:01 +0200

    Form will add books to table

--- javascript.js
+++ javascript.js
@@ -1,51 +1,70 @@
 
 const myLibrary = [];
 addBookToLibrary('title', 'author', 'pages', 'Yes');
-addBookToLibrary('test', 'test', 'test', 'No');
+addBookToLibrary('test', 'test', 124, 'No');
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = myLibrary.length;
 };
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, id) {
+    const book = new Book(title, author, pages, read, id);
+    addBookToTable(book);
     return myLibrary.push(book);
 };
 
-function queryBooks() {
-    const tableBody = document.querySelector('#tableBody');
-    for (const book of myLibrary) {
-        const tr = document.createElement('tr');
+// function queryBooks() {
+//     const tableBody = document.querySelector('#tableBody');
+//     for (const book of myLibrary) {
+//         addBookToTable(book);
+//     }
+// }
 
-        // Create the four td elements
-        const [title, author, pages, read] = Array(4).fill().map(() => {
-            return document.createElement('td');
-        });
+function addBookToTable(book) {
+    const tr = document.createElement('tr');
+    tr.dataset.id = book.id;
 
-        title.innerText = book.title;
-        author.innerText = book.author;
-        pages.innerText = book.pages;
-        read.innerText = book.read;
+    // Create the four td elements
+    const [title, author, pages, read] = Array(4).fill().map(() => {
+        return document.createElement('td');
+    });
 
-        tr.append(title, author, pages, read);
-        tableBody.append(tr);
-    }
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages;
+    read.innerText = book.read;
+
+    tr.append(title, author, pages, read);
+    tableBody.append(tr);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    queryBooks();
 
+    // Open form to create book
     const dialog = document.querySelector('dialog');
     const addBookBtn = document.querySelector('#showForm');
     addBookBtn.addEventListener('click', () => {
         dialog.showModal();
     })
 
+    // Close book
     const closeForm = document.querySelector('#closeForm');
     closeForm.onclick = () => {dialog.close()};
 
-    
+    // Create book
+    const createBook = document.querySelector('#createBook');
+    createBook.addEventListener('click', (e) => {
+        e.preventDefault();
+        const title = dialog.querySelector('#title').value;
+        const author = dialog.querySelector('#author').value;
+        const pages = dialog.querySelector('#pages').value;
+        const read = dialog.querySelector('#read') === 'true' ? "Yes" : "No";
+        addBookToLibrary(title, author, pages, read);
+
+        dialog.close();
+    })
 })
